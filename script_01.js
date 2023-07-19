61dd59a4b39de0dad333aa472d25b91b99bd032f
Nico <nico.aldag@web.de>
2023-07-19 12:44:06 +0200

    1. Iteration non functional

--- script_01.js
+++ script_01.js
@@ -9,19 +9,29 @@
 
 // Parameter --> funktion getSentence([ ...], "Q"){ ...}
 
+// Aufgabe: Satzausgabe mit korrektem Satzzeichen
+
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
 output(getSentence(["Ich","bin","Max"],"S"));
 output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
-output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+output(getSentence(["Ich","hätt","gern","die",
+"Platt","von","dene","zwei","diwodaso","Spass",
+"mache,","habbe","Sie","die"],"Q"));
 
 function getSentence(arr,op) {
-   
-   
-   
-     return "toDo";
+    const GAP = " ";
+    const PUNCT = ".";  // Fall: S
+    const QEST = "?";   // Fall: Q
+    const EXC = "!";    // Falol:E
+    if (op = S) {
+        arr.join(GAP) + PUNCT;
+    }
+    switch
+
+    return arr;
 }
 
 
